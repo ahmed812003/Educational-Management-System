332db92e8dadcf6ba43adc6c74cdf74425efa777
ahmed yaser <ay2309133@gmail.com>
2023-02-12 01:00:46 +0200

    Add files via upload

---
+++ Doctor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalManagementSystem
+{
+    public class Doctor : Human
+    {
+        private List<MainCourse> doctorCourses = new List<MainCourse>();
+
+        public Doctor()
+        {
+
+        }
+
+        public Doctor(string fname, string lname, int id, string username, string password) : base(fname, lname, id, username, password)
+        {
+
+        }
+
+        public List<MainCourse> DoctorCourses
+        {
+            get
+            {
+                return this.doctorCourses;
+            }
+        }
+
+        public MainCourse CreateCourse(string name , string id , string doctor)
+        {
+            MainCourse mainCourse = new MainCourse(name , id , this.FirstName);
+            doctorCourses.Add(mainCourse);
+            return mainCourse;
+        }
+
+        public void CreateNewAssignment (string assignment , MainCourse mainCourse)
+        {
+            mainCourse.AddAssignment(assignment);
+        }
+
+        public void CreateNewAssignment(string assignment, Course course)
+        {
+            course.AddAssignment(assignment);
+        }
+
+        public string GetReportOfAssignments (MainCourse mainCourse)
+        {
+            string report = mainCourse.GetGeneralAssignments();
+            return report;
+        }
+        
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalManagementSystem
+{
+    public class Student : Human
+    {
+
+        private List<Course> courses = new List<Course>();
+
+        public Student()
+        {
+                
+        }
+
+        public Student(string fname , string lname , int id , string username , string password) : base(fname , lname , id , username , password)
+        {
+
+        }
+
+        public List<Course> Courses
+        { 
+            get 
+            { 
+                return this.courses;
+            } 
+        }
+        
+        public void RegisterInCourse (Course course)
+        {
+            courses.Add(course);
+        }
+
+        public void UnRegisterOfCourse (Course course , MainCourse mainCourse)
+        {
+            for(int i=0; i<courses.Count; i++)
+            {
+                if(course.Id == courses[i].Id)
+                {
+                    courses.Remove(courses[i]);
+                }
+            }
+            mainCourse.RemoveStudent(this.Id);
+        }
+
+        public void SubmitAssignment(Course course, string solution, int indexOfAssignment)
+        {
+            course.SumbitSolution(solution, indexOfAssignment-1);
+        }
+
+        public string GetCoursesList()
+        {
+            string CoursesList = "";
+            foreach(var course in this.courses)
+            {
+                CoursesList += course.Id;
+                CoursesList += " ";
+            }
+            return CoursesList;
+        }
+
+        public string GradeReport()
+        {
+            string report = " ";
+            foreach(var course in this.courses)
+            {
+                report += course.GetGradeReport();
+                report += '\n';
+            }
+            return report;
+        }
+
+        public override string ToString()
+        {
+            return $"Student Name : {this.FirstName} {this.LastName}" +
+                   $"Student Id : {this.Id}" +
+                   $"Courses List : {this.GetCoursesList()}";
+        }
+
+    }
+}
